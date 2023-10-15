cdd91d6c34c09ca0deeb99355a9884bdce24a32c
Michael Duren <michaeld@michaelduren.com>
2023-10-15 17:41:16 -0500

    finish chapter 6, remove unecessary project

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,26 @@
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+        fn set_at_table() {}
+    }
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
