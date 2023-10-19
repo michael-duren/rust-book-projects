e8181a8e73f910f1f71f53099227d1f29dde437f
Michael Duren <michaeld@michaelduren.com>
2023-10-19 10:04:17 -0500

    finish chapter 7

---
+++ restaurant/src/front_of_house/hosting.rs
@@ -0,0 +1,8 @@
+pub fn add_to_waitlist() {}
+fn set_at_table() {}
+
+// mod serving {
+//     fn take_order() {}
+//     fn serve_order() {}
+//     fn take_payment() {}
+// }
--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,13 +1,51 @@
-mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-        fn set_at_table() {}
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+fn deliver_order() {}
+
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
     }
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Self {
+            Self {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+// use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast.
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like.
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // The next line won't compile if we uncomment it; we're not
+    // allowed to see or modify the seasonal fruit that comes
+    // with the meal.
+    // meal.seasonal_fruit = String::from("blueberries");
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
 
 pub fn add(left: usize, right: usize) -> usize {
