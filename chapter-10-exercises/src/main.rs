0b1c2a9245bf23f947b7b4c8c33e6308a24638a2
Michael Duren <michaeld@michaelduren.com>
2023-10-22 11:00:12 -0500

    create generic func

--- chapter-10-exercises/src/main.rs
+++ chapter-10-exercises/src/main.rs
@@ -1,3 +1,19 @@
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    return largest;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let largest_num = largest(&number_list);
+
+    println!("The largest number is {}", largest_num);
 }
