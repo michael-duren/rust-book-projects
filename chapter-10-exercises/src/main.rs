0ed21951dc58d899d6984adc0cb11682cedbdc51
Michael Duren <michaeld@michaelduren.com>
2023-10-23 10:00:05 -0500

    update to ch 10

--- chapter-10-exercises/src/main.rs
+++ chapter-10-exercises/src/main.rs
@@ -1,3 +1,49 @@
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+    // default impl
+    // do not have to implement summarize unless you want to
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("{}, by {}, ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        return format!("{}, by {}", self.content, self.username);
+    }
+}
+
+// use trait as func parameter
+// pub fn notify(item: &impl Summary) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+// the above works for straightforward cases, but is actually
+// syntactic sugar for the below
+pub fn notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
 fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
